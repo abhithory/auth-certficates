62e8e6a6e976e08fa86d3cc54f3cf1a29d40d94a
abhithory <abhithory@gmail.com>
2023-11-08 04:51:13 -0600

    nft smart contract added

---
+++ SmartContract/CertificateTokenNft.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract CertificateNftTokens is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId = 0;
+
+    constructor()
+        ERC721("CertificateTokens", "CT")
+        Ownable(msg.sender)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
