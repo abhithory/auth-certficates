5006ca150584bceeba89c6f036e3a72565ca6042
abhithory <abhithory@gmail.com>
2023-11-09 05:41:31 -0600

    certificate Number short

--- src/app/api/certificate/route.ts
+++ src/app/api/certificate/route.ts
@@ -3,6 +3,10 @@ import Certificate from '@/database/models/CertificateModel';
 import { createCertificateZSchema } from '@/utils/zSchema/CertificateValidations';
 import { NextRequest, NextResponse } from 'next/server'
 
+import ShortUniqueId from 'short-unique-id';
+
+
+const { randomUUID } = new ShortUniqueId({ length: 16 });
 
 export async function POST(req: NextRequest) {
     try {
@@ -18,7 +22,14 @@ export async function POST(req: NextRequest) {
         const _certificate = await Certificate.findOne({ recipientEmail: validation.data.recipientEmail.toLowerCase() });
         if (_certificate) return NextResponse.json({ status: true, message: "Certificate Already Exits with this email address", data: _certificate }, { status: 200 });
 
-        const newPrompt = new Certificate({ recipientName: validation.data.recipientName, recipientEmail: validation.data.recipientEmail.toLowerCase() });
+
+        const generatedCertificateNumber = randomUUID();
+        const certificateNumber = String(generatedCertificateNumber).toLowerCase();
+
+        console.log({ certificateNumber });
+
+
+        const newPrompt = new Certificate({ certificateNumber, recipientName: validation.data.recipientName, recipientEmail: validation.data.recipientEmail.toLowerCase() });
         await newPrompt.save();
         return NextResponse.json({ status: true, message: "Certificate created succefully", data: newPrompt }, { status: 201 })
     } catch (error: any) {
--- src/database/models/CertificateModel.ts
+++ src/database/models/CertificateModel.ts
@@ -1,12 +1,12 @@
 import { Schema, model, models } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
+
 
 
 const CertificateSchema = new Schema({
     certificateNumber: {
         type: String,
         unique: [true, 'Certificate Number already exists!'],
-        default: uuidv4
+        required: [true, "Certificate number is required"]
     },
     recipientName: {
         type: String,
@@ -25,18 +25,6 @@ const CertificateSchema = new Schema({
         default: Date.now,
     },
 });
-
-// CertificateSchema.pre('save', async function (next) {
-//     console.log("isNew", this.isNew);
-
-//     if (!this.isNew) {
-//         return next();
-//     }
-//     const generatedCertificateNumber = uuidv4();
-//     this.certificateNumber = String(generatedCertificateNumber).toLowerCase();
-//     console.log("generatedCertificateNumber", generatedCertificateNumber);
-//     next();
-// });
 
 const Certificate = models.Certificate || model('Certificate', CertificateSchema);
 
